4813f2c6a1b01de08ba8e6b32b36a6be3ba47b76
dygduck <duyguebcim@yahoo.com>
2018-08-19 21:23:12 -0700

    accessibility improvement.

--- js/main.js
+++ js/main.js
@@ -163,9 +163,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `${restaurant.name} Restaurant`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -180,6 +181,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('aria-label', `More details about ${restaurant.name}`)
   li.append(more)
 
   return li
